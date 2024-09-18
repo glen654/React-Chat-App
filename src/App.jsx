f4a51e180fb966136786caa295d2700e39fe24ef
Glen Alloy <glenalloy8@gmail.com>
2024-09-18 19:11:05 +0530

    mounted components to the app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import List from './components/list/List'
+import Chat from './components/chat/Chat'
+import Detail from './components/detail/Detail'
 
 const App = () => {
   return (
     <div className='container'>
-      
+        <List/>
+        <Chat/>
+        <Detail/>
     </div>
   )
 }
