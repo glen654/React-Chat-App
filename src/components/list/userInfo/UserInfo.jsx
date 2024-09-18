50b8901322007682909cf780488faace3b2ec1f1
Glen Alloy <glenalloy8@gmail.com>
2024-09-18 19:56:23 +0530

    implemented user info styles

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import './UserInfo.css'
 
+
 const UserInfo = () => {
   return (
     <div className='userInfo'>
         <div className="user">
-            <img src="./avatar.png" alt="" />
+            <img src="../../../../public/assets/avatar.svg" alt="avatar image" />
             <h2>John Doe</h2>
         </div>
         <div className="icons">
-            <img src="./more" alt="" />
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img src="../../../../public/assets/more.svg" alt="more image" />
+            <img src="../../../../public/assets/video.svg" alt="video image" />
+            <img src="../../../../public/assets/edit.svg" alt="edit image" />
         </div>
     </div>
   )
