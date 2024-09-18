bc976de7e077676b2ba4f4b63ad5c0eeff5ee3c7
Glen Alloy <glenalloy8@gmail.com>
2024-09-18 19:27:48 +0530

    imported react icons to the project

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -3,7 +3,17 @@ import './UserInfo.css'
 
 const UserInfo = () => {
   return (
-    <div className='userInfo'>UserInfo</div>
+    <div className='userInfo'>
+        <div className="user">
+            <img src="./avatar.png" alt="" />
+            <h2>John Doe</h2>
+        </div>
+        <div className="icons">
+            <img src="./more" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+    </div>
   )
 }
 
