d305c9cec573af28d8e75a8c88be19effaa34fbd
Glen Alloy <glenalloy8@gmail.com>
2024-09-19 11:10:02 +0530

    implemented styles to the search bar

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './chatList.css'
 
 const ChatList = () => {
+  const [addMode,setAddMode] = useState(false);
   return (
     <div className='chatList'>
         <div className="search">
@@ -9,7 +10,11 @@ const ChatList = () => {
                 <img src="../../../../public/assets/search.svg" alt="" />
                 <input type="text" placeholder='Search'/>
            </div>
-           <img src="../../../../public/assets/plus.svg" alt="" />
+           <img src={addMode ? "../../../../public/assets/minus.svg" : "../../../../public/assets/plus.svg"} alt="" className='add' 
+           onClick={() => setAddMode((prev) => !prev)}/>
+        </div>
+        <div className="item">
+          
         </div>
     </div>
   )
