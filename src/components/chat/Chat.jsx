bbd242316e385aeedf416367762ec4eb363568bc
Glen Alloy <glenalloy8@gmail.com>
2024-09-19 12:15:35 +0530

    imported emoji png to the projects

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,7 +3,30 @@ import './Chat.css'
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="top">
+        <div className="user">
+          <img src="../../../public/assets/avatar.svg" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor, sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="../../../public/assets/phone.png" alt="" />
+          <img src="../../../public/assets/video.png" alt="" />
+          <img src="../../../public/assets/info.png" alt="" />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom">
+        <div className="icons"></div>
+        <input type="text" placeholder='Type a message...'/>
+        <div className="emoji">
+          <img src="../../../public/assets/emoji.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
