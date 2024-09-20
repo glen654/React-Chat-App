edf82ad80198eeaba936ef0b12e2f5480cf0a27f
Glen Alloy <glenalloy8@gmail.com>
2024-09-20 12:48:10 +0530

    implemented login component

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,7 +3,23 @@ import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="login">
+        <img src="../../../public/assets/chat.png" alt="" />
+        <form className='login-form'>
+            <h2>Sign Up</h2>
+            <input type="text" placeholder="Username" className="form-input" required/>
+            <input type="email" placeholder='Email address' className="form-input" />
+            <input type="password" placeholder='password' className="form-input" />
+            <button type='submit'>Sign Up</button>
+            <div className="login-term">
+                <input type="checkbox" />
+                <p>Agree to the terms of use & privacy policy.</p>
+            </div>
+            <div className="login-forgot">
+                <p className="login-toggle">Already have an account <span>click here</span></p>
+            </div>
+        </form>
+    </div>
   )
 }
 
