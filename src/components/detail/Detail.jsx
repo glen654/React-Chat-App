f2abaeb2135156a07a69cb26a76e29b129b7b364
Glen Alloy <glenalloy8@gmail.com>
2024-09-20 11:12:44 +0530

    implemented detail component

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -29,18 +29,42 @@ const Detail = () => {
           </div>
           <div className="photos">
             <div className="photoItem">
-              <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?w=1380&t=st=1726809724~exp=1726810324~hmac=6acb917b719e43caeb8badb4f2cec2b5d7623334833a49b335de2e6a7d46b6fb" alt="" />
-              <span>photo_2024_2.png</span>
+              <div className="photoDetail">
+                <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?w=1380&t=st=1726809724~exp=1726810324~hmac=6acb917b719e43caeb8badb4f2cec2b5d7623334833a49b335de2e6a7d46b6fb" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+             <img src="../../../public/assets/download.png" alt="" />
             </div>
-            <img src="../../../public/assets/download.png" alt="" />
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?w=1380&t=st=1726809724~exp=1726810324~hmac=6acb917b719e43caeb8badb4f2cec2b5d7623334833a49b335de2e6a7d46b6fb" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+             <img src="../../../public/assets/download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?w=1380&t=st=1726809724~exp=1726810324~hmac=6acb917b719e43caeb8badb4f2cec2b5d7623334833a49b335de2e6a7d46b6fb" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+             <img src="../../../public/assets/download.png" alt="" />
+            </div>
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img src="https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?w=1380&t=st=1726809724~exp=1726810324~hmac=6acb917b719e43caeb8badb4f2cec2b5d7623334833a49b335de2e6a7d46b6fb" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+             <img src="../../../public/assets/download.png" alt="" />
+            </div>
           </div>
         </div>
         <div className="option">
           <div className="title">
-            <span>Chat Settings</span>
+            <span>Shared Files</span>
             <img src="../../../public/assets/arrowUp.png" alt="" />
           </div>
         </div>
+        <button>Block User</button>
       </div>
     </div>
   )
