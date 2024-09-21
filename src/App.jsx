b4694a9d273623bff38ed91a1ca5b61576500274
Glen Alloy <glenalloy8@gmail.com>
2024-09-21 12:58:36 +0530

    implemented the form handel submit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import List from './components/list/List'
 import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
 import Login from './components/login/Login'
+import Notification from './components/notification/Notification'
 
 const App = () => {
 
-  const user = false
+  const user = true
   return (
     <div className='container'>
       {
@@ -17,9 +18,12 @@ const App = () => {
             <Detail/>
           </>
           
-        ) : (<Login />)
-      }
-        
+        ) : (
+           
+           <Login />
+
+        )}
+        <Notification/>
 
     </div>
   )
