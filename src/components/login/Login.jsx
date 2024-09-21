498b0ed1b86ad8a86e4cf1de017651f484debfa7
Glen Alloy <glenalloy8@gmail.com>
2024-09-21 12:47:25 +0530

    imported react toastify

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 const Login = () => {
+  const [avatar,setAvatar] = useState({
+    file:null,
+    url:""
+  })
+
+  const handleAvatar = (event) => {
+    if(event.target.files[0]){
+      setAvatar({
+        file:event.target.files[0],
+        url:URL.createObjectURL(event.target.files[0])
+      })
+    }
+    
+  }
   return (
-    <div>
-      login
+    <div className='login'>
+      <div className="item">
+        <h2>Welcome back,</h2>
+        <form>
+          <input type="text" placeholder='Email' name='email'/>
+          <input type="password" placeholder='Password' name='password'/>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="seperator"></div>
+      <div className="item">
+      <h2>Create an Account</h2>
+        <form>
+          <label htmlFor="file">
+            <img src={avatar.url || '../../../public/assets/avatar.svg'} alt="" />
+            Upload an image</label>
+          <input type="file" id='file' style={{display:"none"}} onChange={handleAvatar}/>
+          <input type="text" placeholder='Username' name='username'/>
+          <input type="text" placeholder='Email' name='email'/>
+          <input type="password" placeholder='Password' name='password'/>
+          <button>Sign Up</button>
+        </form>
+      </div>
     </div>
   )
 }
