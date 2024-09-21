1fe9f89982c4c7ab57abd1116e179b56786ebc1a
Glen Alloy <glenalloy8@gmail.com>
2024-09-21 12:08:58 +0530

    changed the favicon of the project

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,27 @@ import List from './components/list/List'
 import Chat from './components/chat/Chat'
 import Detail from './components/detail/Detail'
 import Login from './components/login/Login'
-import {Route,Routes} from 'react-router-dom'
-
-// const App = () => {
-
-//   const user = false
-//   return (
-//     <div className='container'>
-//       {
-//         user ? (
-//           <>
-//             <List/>
-//             <Chat/>
-//             <Detail/>
-//           </>
-          
-//         ) : (<Login />)
-//       }
-        
-
-//     </div>
-//   )
-// }
 
 const App = () => {
+
+  const user = false
   return (
-    <>
-      <Routes>
-         <Route path='/' element={<Login />}/>
-         <Route path='/chat' element={<Chat />}/>
-         <Route path='/detail' element={<Detail />}/>
-         <Route path='/list' element={<List />}/>
-      </Routes>
-    </>
+    <div className='container'>
+      {
+        user ? (
+          <>
+            <List/>
+            <Chat/>
+            <Detail/>
+          </>
+          
+        ) : (<Login />)
+      }
+        
+
+    </div>
   )
 }
+
 
 export default App
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,22 +3,8 @@ import './Login.css'
 
 const Login = () => {
   return (
-    <div className="login">
-        <img src="../../../public/assets/meetme.png" alt="" />
-        <form className='login-form'>
-            <h2>Sign Up</h2>
-            <input type="text" placeholder="Username" className="form-input" required/>
-            <input type="email" placeholder='Email address' className="form-input" required/>
-            <input type="password" placeholder='password' className="form-input" required/>
-            <button type='submit'>Sign Up</button>
-            <div className="login-term">
-                <input type="checkbox" />
-                <p>Agree to the terms of use & privacy policy.</p>
-            </div>
-            <div className="login-forgot">
-                <p className="login-toggle">Already have an account <span>click here</span></p>
-            </div>
-        </form>
+    <div>
+      login
     </div>
   )
 }
